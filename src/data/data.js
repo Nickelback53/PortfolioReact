de926feda11d438230028b8f2aea8181377f1041
taeho <taeho0523@gmail.com>
2022-10-19 13:13:52 +0900

    update2

---
+++ src/data/data.js
@@ -0,0 +1,166 @@
+let data = {
+    language: [
+        {
+            name: 'Python',
+            color: '3776AB',
+        },
+        {
+            name: 'java',
+            color: 'orange',
+        },
+        {
+            name: 'JavaScript',
+            color: 'F7DF1E',
+        },
+        {
+            name: 'C Sharp',
+            color: '239120',
+        }
+    ],
+    webDev: [
+        {
+            name: 'Node.js',
+            color: '339933',
+        },
+        {
+            name: 'Express.js',
+            color: 'orange',
+        },
+        {
+            name: 'React',
+            color: '61DAFB',
+        },
+        {
+            name: 'Vue.js',
+            color: '4FC08D',
+        },
+        {
+            name: 'Spring Boot',
+            color: '6DB33F',
+        }
+    ],
+    database: [
+        {
+            name: 'MySQL',
+            color: '4479A1',
+        },
+        {
+            name: 'MariaDB',
+            color: '003545',
+        },
+        {
+            name: 'Oracle',
+            color: 'F80000',
+        }
+    ],
+    etc: [
+        {
+            name: 'GitHub',
+            color: '181717',
+        },
+        {
+            name: 'Docker',
+            color: '2496ED',
+        },
+        {
+            name: 'CentOS',
+            color: '262577',
+        },
+        {
+            name: 'Amazon AWS',
+            color: 'FF9900',
+        }
+    ],
+    project: [
+        [
+            {
+                title: 'github',
+                decription: 'https://github.com/Nickelback53/shop',
+            },
+            {
+                title: 'url',
+                decription: 'https://nickelback53.github.io/',
+            },
+            {
+                title: 'frontend',
+                decription: 'React',
+            }
+        ],
+        [
+            {
+                title: 'github',
+                decription: 'https://github.com/solduma/EldenSquare',
+            },
+            {
+                title: 'Platform',
+                decription: 'Unity',
+            }
+        ],
+        [
+            {
+                title: 'github',
+                decription: 'https://github.com/Nickelback53/PortfolioReact',
+            },
+            {
+                title: 'url',
+                decription: 'http://nickelback53.synology.me:58000/',
+            },
+            {
+                title: 'frontend',
+                decription: 'React',
+            }
+        ],[
+            {
+                title: 'github',
+                decription: 'https://github.com/Nickelback53/nextJs-PUBGStat',
+            },
+            {
+                title: 'url',
+                decription: 'https://nextjs-pubgstat.vercel.app/',
+            },
+            {
+                title: 'frontend/backend',
+                decription: 'nextJs(React/node)',
+            },
+        ]
+
+    ],
+    Employment:{
+        ko:[
+            {period: '2020-2022',
+            Corporate: '야인소프트',
+            project:[
+                '고려대학교 Datawarehouse 사업 참여\n',
+                '철도청 고장/수리 통계화 사업 참여\n',
+                '인사혁신처 BI모델 교체 사업 참여\n'
+            ]
+        }
+        ],
+        en:[
+            {period: '2020-2022',
+            Corporate: 'yainsoft',
+            project:[
+                `Participate in Korea University's Datawarehouse project\n`,
+                'Participation in the National Railroad Breakdown/Repair Statistics Project\n',
+                'Participation in the BI model replacement project of the Ministry of Personnel Management\n'
+            ]
+        }
+        ],
+        jp:[
+            {period: '2020-2022',
+            Corporate: 'ヤインソフト',
+            project:[
+                '高麗大学データウェアハウスプロジェクトに参加\n',
+                '鉄道庁故障/修理統計化事業に参加\n',
+                '人事革新処BIモデル交代事業に参加\n'
+            ]
+        }
+        ],
+    }
+
+}
+
+
+
+
+export default data;
